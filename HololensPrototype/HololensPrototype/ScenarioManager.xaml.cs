bb9f09fd598fbfa562e4f866a0cb1d63bbba4274
jastuart0412 <jastuart@iastate.edu>
2016-11-15 09:46:06 -0600

    Upload Code
    Added code to upload survey files to the database.

--- HololensPrototype/HololensPrototype/ScenarioManager.xaml.cs
+++ HololensPrototype/HololensPrototype/ScenarioManager.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,7 +34,37 @@ namespace HololensPrototype
 
             if(file==true)
             {
+                byte[] fileBytes = File.ReadAllBytes(@dialogWindow.FileName);
+                int fileSize = Convert.ToInt32(new FileInfo(dialogWindow.FileName).Length);
+                string fileName = dialogWindow.FileName.Substring(dialogWindow.FileName.LastIndexOf("\\") + 1);
 
+                using (MySqlConnection conn = new MySqlConnection("ENTER SQL CONNECTION HERE"))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "Delete * FROM Scenarios WHERE fileName='" + fileName + "';";
+                        cmd.ExecuteNonQuery();
+
+                        cmd.CommandText = "INSERT INTO Scenarios (fileName, fileSize, file) VALUES (?fileName, ?fileSize, ?fileBytes);";
+                        MySqlParameter fName = new MySqlParameter("?fileName", MySqlDbType.VarChar, 256);
+                        MySqlParameter fSize = new MySqlParameter("?fileSize", MySqlDbType.Int32, 11);
+                        MySqlParameter fContent = new MySqlParameter("?fileBytes", MySqlDbType.Blob, fileBytes.Length);
+
+                        fName.Value = fileName;
+                        fSize.Value = fileSize;
+                        fContent.Value = fileBytes;
+
+                        cmd.Parameters.Add(fName);
+                        cmd.Parameters.Add(fSize);
+                        cmd.Parameters.Add(fContent);
+
+                        conn.Open();
+
+                        cmd.ExecuteNonQuery();
+
+                    }
+                }
             }
         }
     }
--- HololensPrototype/HololensPrototype/SurveyManager.xaml.cs
+++ HololensPrototype/HololensPrototype/SurveyManager.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.IO;
 
 namespace HololensPrototype
 {
@@ -32,8 +34,48 @@ namespace HololensPrototype
 
             if (file == true)
             {
+                byte[] fileBytes = File.ReadAllBytes(@dialogWindow.FileName);
+                int fileSize = Convert.ToInt32(new FileInfo(dialogWindow.FileName).Length);
+                string fileName = dialogWindow.FileName.Substring(dialogWindow.FileName.LastIndexOf("\\") + 1);
 
+                using (MySqlConnection conn = new MySqlConnection("Server=mysql-509.cs.iastate.edu; Database=db509t03;User Id=dbu509t03;Password = zebr8p@AgEsU; "))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        conn.Open();
+                        cmd.CommandText = "Delete FROM Survey WHERE fileName='" + fileName + "';";
+                        cmd.ExecuteNonQuery();
+
+                        cmd.CommandText = "INSERT INTO Survey (fileName, fileSize, fileBytes) VALUES (?fileName, ?fileSize, ?fileBytes);";
+                        MySqlParameter fName = new MySqlParameter("?fileName", MySqlDbType.VarChar, 256);
+                        MySqlParameter fSize = new MySqlParameter("?fileSize", MySqlDbType.Int32, 11);
+                        MySqlParameter fContent = new MySqlParameter("?fileBytes", MySqlDbType.Blob, fileBytes.Length);
+
+                        fName.Value = fileName;
+                        fSize.Value = fileSize;
+                        fContent.Value = fileBytes;
+
+                        cmd.Parameters.Add(fName);
+                        cmd.Parameters.Add(fSize);
+                        cmd.Parameters.Add(fContent);
+
+                        cmd.ExecuteNonQuery();
+
+                    }
+                }
             }
         }
+
+        private void formLoad(object sender, RoutedEventArgs e)
+        {
+            SurveyGrid.Items.Add(new SurveyTemplate { surveyName = "Test", uploadDate = "Test Date" });
+        }
+    }
+
+    public class SurveyTemplate
+    {
+        public string surveyName { get; set; }
+        public string uploadDate { get; set; }
     }
 }
